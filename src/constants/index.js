42d4cf703741ee321c148d6b79d69c9dbd5c4cd5
ahnika <98440824+AhnikaRamachandra@users.noreply.github.com>
2025-07-07 01:06:51 +0530

    imaage

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import img1 from '../assets/showbeat.png'
 import img2 from '../assets/figma1.png'
 import edueval from '../assets/eduevals.png'
-import design from '../assets/designsystem.png'
+import design from '../assets/Designsystem.png'
 import weather from '../assets/weather.png'
 import chic from '../assets/chic.png'
 export const PROJECTS = [
@@ -12,21 +12,20 @@ export const PROJECTS = [
     technologies: ["Figma", "Canva"],
     image:img1,
     link:"/Beatflow"      
-  },  {
+  },
+   {
+  title: "UI Design System",
+  description: "A reusable, scalable UI design system built using Figma. It includes components like buttons, checkboxes, form fields, modals, and a consistent color & typography style guide to ensure visual consistency across products.",
+  technologies: ["Figma", "Auto Layout", "Variants", "Typography", "Color Tokens"],
+  image: design,
+  link: "https://www.figma.com/design/d4X5ZrlgyAQI87aDfqDY2v/icons?node-id=0-1&t=imDAYUIfl6Qxve94-1" 
+}, {
     title: "Bitecraft",
     description: "Designed an user interface using Figma, focusing on user experience and accessibility.",
     technologies: ["Figma", "Canva"],
     image:img2,
     link: "/figma1"
   },
-  {
-  title: "UI Design System",
-  description: "A reusable, scalable UI design system built using Figma. It includes components like buttons, checkboxes, form fields, modals, and a consistent color & typography style guide to ensure visual consistency across products.",
-  technologies: ["Figma", "Auto Layout", "Variants", "Typography", "Color Tokens"],
-  image: design,
-  link: "https://www.figma.com/design/d4X5ZrlgyAQI87aDfqDY2v/icons?node-id=0-1&t=imDAYUIfl6Qxve94-1" 
-}
-,
     {
       title: "Chic & Shine",
       description: "A stylish and user-friendly e-commerce platform built using HTML, CSS, and JavaScript.",
